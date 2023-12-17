269226e652588a69bdbbceec7a51a6ae9560d87d
GraphicSquare23 <graphicsquare23@gmail.com>
2023-12-17 09:27:49 +0530

    changes on contact us modal and controller

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -1,18 +1,24 @@
-const Contact = require('../models/contact');
+const Contact = require("../models/contact");
 
 exports.submitContact = async (req, res) => {
   try {
-    const {name,email,project_type,message} = req.body;
+    const { name, email, project_type, message, phone, whatapp, city } =
+      req.body;
     const newContact = new Contact({
-        name,
-        email,
-        project_type,
-        message
-      });
-  
-      // Save the record to the database
-      await newContact.save({ collection: "contacts" });
-      res.status(200).json({ message: 'Record inserted successfully', status: 200 });
+      name,
+      email,
+      phone,
+      whatapp,
+      city,
+      project_type,
+      message,
+    });
+
+    // Save the record to the database
+    await newContact.save({ collection: "contacts" });
+    res
+      .status(200)
+      .json({ message: "Record inserted successfully", status: 200 });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
--- src/models/contact.js
+++ src/models/contact.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const contactSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  project_type: { type: String, required: true },
-  message: { type: String, required: true }
+  phone: { type: String },
+  whatapp: { type: String },
+  city: { type: String },
+  project_type: { type: String },
+  message: { type: String },
 });
 
-module.exports = mongoose.model('contacts', contactSchema);
+module.exports = mongoose.model("contacts", contactSchema);
